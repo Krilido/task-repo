b6b85ef8830afe33dc31016071d6c4694afdeadc
krilido <raga@wowrack.com>
2020-12-20 05:47:36 +0700

    init for model, migration and access by localhost

---
+++ app/Model/Section.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Section extends Model
+{
+    const ACTIVE = 1;
+    const NON_ACTIVE = 0;
+}
---
+++ app/Model/Task.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    const ACTIVE = 1;
+    const NON_ACTIVE = 0;
+}
